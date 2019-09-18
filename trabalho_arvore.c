0bf0132c8f2d328d026ac5a50525885a21ec38b9
Ruan <droderuan@hotmail.com>
2019-09-18 18:33:16 -0300

    Resolvido o problema de inserção e add preOrdem

--- trabalho_arvore.c
+++ trabalho_arvore.c
@@ -10,33 +10,56 @@ typedef struct no {
 no* buscarElemento(no *no, int x, int *f);
 no* inserirElemento(no *no, int x);
 
+void preOrdem(no *arvore);
+
 int main(){
-	no *no = NULL;
+	no *raiz = NULL;
+	int x;
+	preOrdem(raiz);
 	
+	printf("\ndigite: ");
+	scanf("%d", &x);
+	if(raiz == NULL){
+	raiz = inserirElemento(raiz, x);
+	}
+	printf("\ndigite: ");
+	scanf("%d", &x);
+	inserirElemento(raiz, x);
+	
+	printf("\ndigite: ");
+	scanf("%d", &x);
+	inserirElemento(raiz, x);
+	
+	preOrdem(raiz);
 	return 0;
 }
 
-no* buscarElemento(no *no, int x, int *f){
-	if(no == NULL){             //verifica se o nó raiz (nó de onde a busca irá partir) é null ou se contem o próprio elemento
+no* buscarElemento(no *NO, int x, int *f){
+	printf("\n\nbuscando na arvore...");
+	if(NO == NULL){             //verifica se o nó raiz (nó de onde a busca irá partir) é null ou se contem o próprio elemento
 		*f = 0;
+		printf("\narvore vazia\n\n");
 		return NULL;
-	} else if(no->data == x){
+	} else if(NO->data == x){
 		*f = 1;
-		return no;
+		printf("\nelemento ja inserido\n\n");
+		return NO;
 	}
-	if (x < no->data){			//verifica se a busca vai para a esquerda e se o filho da esquerda é null, se não entra numa outra chamada recursiva
-		if (no->filho_esq == NULL){
+	if (x < NO->data){			//verifica se a busca vai para a esquerda e se o filho da esquerda é null, se não entra numa outra chamada recursiva
+		if (NO->filho_esq == NULL){
 			*f = 2;
-			return no;
+			printf("\nFolha encontrado!\n\n");
+			return NO;
 		} else{
-			buscarElemento(no->filho_esq, x, f);
+			buscarElemento(NO->filho_esq, x, f);
 		}
 	}else{
-		if( no->filho_dir == NULL){			//verifica se a busca vai para a direita e se o filho da direita é null, se não entra numa outra chamada recursiva
+		if( NO->filho_dir == NULL){			//verifica se a busca vai para a direita e se o filho da direita é null, se não entra numa outra chamada recursiva
 			*f = 2;
-			return no;
+			printf("\nelemento encontrado!\n\n", NO->data);
+			return NO;
 		} else{
-			buscarElemento(no->filho_dir, x, f);
+			buscarElemento(NO->filho_dir, x, f);
 		}
 	}
 }
@@ -49,19 +72,28 @@ no* inserirElemento(no *NO, int x){  //nao usar o nome da variavel com a mesma d
 	if(f == 1){
 		printf("elemento já inserido");
 		return NULL;
-	}
-	newNo = malloc(sizeof(no));
-	newNo->data = x;
-	newNo->filho_dir = NULL;
-	newNo->filho_esq = NULL;
-	
-	if(f == 0){
-		return newNo;
-	} else if(f == 2){
-		if(newNo->data < NO->data){
-			NO->filho_esq = newNo;
-		}else{
-			NO->filho_dir = newNo;
+	} else {
+		newNo = malloc(sizeof(no));
+		newNo->data = x;
+		newNo->filho_dir = NULL;
+		newNo->filho_esq = NULL;	
+		if(f == 0){
+			printf("elemento inserido na raiz\n\n");
+			return newNo;		
+		} else if(f == 2){
+			if(newNo->data < aux->data){
+				aux->filho_esq = newNo;
+			}else{
+				aux->filho_dir = newNo;
+			}
 		}
+	}
+}
+
+void preOrdem(no *arvore){
+	if(arvore != NULL){
+		printf("%d\n", arvore->data);
+		preOrdem(arvore->filho_esq);
+		preOrdem(arvore->filho_dir);
 	}
 }
